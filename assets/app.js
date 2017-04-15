d96731f7ce853913e53236c8243ab40128fb9ae1
Chase-UCF <chaserobison8@gmail.com>
2017-04-14 22:43:01 -0400

    final attempt

--- assets/app.js
+++ assets/app.js
@@ -49,71 +49,25 @@ database.ref().on("child_added", function(snapshot) {
     console.log(snapshot.val().frequency);
     //get last child from db and place in html attribute
     var firstTrainTime = snapshot.val().firstTrain;
-    var trainTime = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-    console.log(firstTrainTime);
+    var frequencyToBe = snapshot.val().frequency.toString;
+    var currentTime = moment();
 
     var diffTime = moment().diff(moment(firstTrainTime), "minutes");
     console.log("DIFFERENCE IN TIME: " + diffTime);
 
+    var tRemainder = diffTime % frequencyToBe;
+    console.log(tRemainder);
+    // Minute Until Train
+    var tMinutesTillTrain = frequencyToBe - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
     $("#tbody").append("<tr>" + "<td>" + snapshot.val().trainName +
     "</td><td>" + snapshot.val().destination +
     "</td><td>" + snapshot.val().frequency +
-    "</td><td>" + firstTrainTime + "</td><td>" + diffTime + "</td>");
+    "</td><td>" + firstTrainTime + "</td><td>" + tMinutesTillTrain + "</td>");
 
 // Handle the errors
 },
 function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });
-
-    // Assume the following situations.
-    // (TEST 1)
-    // First Train of the Day is 3:00 AM
-    // Assume Train comes every 3 minutes.
-    // Assume the current time is 3:16 AM....
-    // What time would the next train be...? (Use your brain first)
-    // It would be 3:18 -- 2 minutes away
-    // (TEST 2)
-    // First Train of the Day is 3:00 AM
-    // Assume Train comes every 7 minutes.
-    // Assume the current time is 3:16 AM....
-    // What time would the next train be...? (Use your brain first)
-    // It would be 3:21 -- 5 minutes away
-    // ==========================================================
-    // Solved Mathematically
-    // Test case 1:
-    // 16 - 00 = 16
-    // 16 % 3 = 1 (Modulus is the remainder)
-    // 3 - 1 = 2 minutes away
-    // 2 + 3:16 = 3:18
-    // Solved Mathematically
-    // Test case 2:
-    // 16 - 00 = 16
-    // 16 % 7 = 2 (Modulus is the remainder)
-    // 7 - 2 = 5 minutes away
-    // 5 + 3:16 = 3:21
-    // Assumptions
-    var tFrequency = 3;
-    // Time is 3:30 AM
-    var firstTime = "03:30";
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
-    // Current Time
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-    // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
-    // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-    var trainTime = moment(firstTrain, "hh:mm").subtract(1, "years");
-    console.log(firstTrain);
